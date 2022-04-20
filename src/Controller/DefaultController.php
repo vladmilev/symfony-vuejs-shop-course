f17c01d01f23d317fee2b03e0d4d65f5790e0c7b
Vlad Milevsky <vlad_milev@mail.ru>
2022-04-20 18:24:39 +0700

    m10 postgres added

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="app_default")
+     * @Route("/", name="homepage")
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $productList = $em->getRepository(Product::class)->findAll();
+        dd($productList);
+
         return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/product-add", name="product_add")
+     */
+    public function productAdd(): Response
+    {
+        $product = new Product();
+        $product->setTitle( "Product-".rand(1,100));
+        $product->setDescription("smth");
+        $product->setPrice(10);
+        $product->setQuantity(1);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return $this->redirectToRoute("homepage");
     }
 }
